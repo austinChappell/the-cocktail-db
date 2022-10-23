0fa2b669cd321c4b1598e334c945a6df1149ef02
Austin Chappell <austin.w.chappell@gmail.com>
2022-10-23 01:14:35 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+import { Drink } from "./types";
+
+interface ConstructorArgs {
+  apiKey: string;
+  version: number;
+}
+
+export class CocktailDbApi {
+  constructor({
+    apiKey,
+    version,
+  }: ConstructorArgs) {
+    if (version !== 1) {
+      throw new Error('Unsupported version number. The supported version number is: 1');
+    }
+
+    if (!apiKey) {
+      throw new Error('API key required.')
+    }
+
+    this.apiKey = apiKey;
+    this.version = version;
+  }
+
+  private readonly apiKey: string;
+  private readonly version: number;
+
+  private fetchDataFromService = async <T>(url: string) => {
+    const baseUrl = `https://www.thecocktaildb.com/api/json/v${this.version}/${this.apiKey}`;
+
+    return axios.get<T>(`${baseUrl}/${url}`);
+  }
+
+  searchCocktailsByName = async (searchTerm: string) => {
+    const encodedSearchTerm = encodeURIComponent(searchTerm);
+
+    return this.fetchDataFromService<{ drinks: Drink[] }>(`/search.php?s=${encodedSearchTerm}`)
+  }
+}
